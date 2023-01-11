c002f4fbb08c1bcd960aab24bfb1bb95d044d8b3
emrahbeydilli <emrahbeydilli@gmail.com>
2023-01-11 07:03:36 +0300

    updating firebase setup

--- src/routes/signin/sign-in.component.jsx
+++ src/routes/signin/sign-in.component.jsx
@@ -1,16 +1,19 @@
 import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-   const logGoogleUser = async () =>{
+   const logGoogleUser = async () => {
       const response = await signInWithGooglePopup();
       console.log(response);
    }
-   
-    return ( 
-        <div>
-           <h1>Sign In Page</h1> 
-        </div>
-     );
+
+   return (
+      <div>
+         <h1>Sign In Page</h1>
+         <button onClick={logGoogleUser}>
+            Sign in with Google
+         </button>
+      </div>
+   );
 }
- 
+
 export default SignIn;
