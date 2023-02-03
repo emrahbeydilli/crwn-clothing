5c8a8729b013e44d677ba7f3706df12b49659d11
emrahbeydilli <emrahbeydilli@gmail.com>
2023-02-04 00:44:46 +0300

    updating authention method

--- src/routes/signin/sign-in.component.jsx
+++ src/routes/signin/sign-in.component.jsx
@@ -1,26 +1,9 @@
-import { useEffect } from "react";
-import { getRedirectResult, onAuthStateChanged, signInWithRedirect } from "firebase/auth";
-
 import {
-   auth,
    signInWithGooglePopup,
-   signInWithGoogleRedirect,
    createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
-import { async } from "@firebase/util";
-
 
 const SignIn = () => {
-
-   useEffect(()=> {
-      const createUser = async()=>{
-         const response = await getRedirectResult(auth);
-         if (response) {
-            const userDocRef = await createUserDocumentFromAuth(response.user);
-         }
-      }
-      createUser();
-   },[])
 
    const logGoogleUser = async () => {
       const {user} = await signInWithGooglePopup();
@@ -32,9 +15,6 @@ const SignIn = () => {
          <h1>Sign In Page</h1>
          <button onClick={logGoogleUser}>
             Sign in with Google
-         </button>
-         <button onClick={signInWithGoogleRedirect}>
-            Sign in with Google Redirect
          </button>
       </div>
    );
