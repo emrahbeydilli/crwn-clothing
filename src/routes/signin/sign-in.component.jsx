879e3201ea1fcb7ce0ceba7f4bd44eff513bfe5e
emrahbeydilli <emrahbeydilli@gmail.com>
2023-02-19 20:24:39 +0300

    adding sign up form

--- src/routes/signin/sign-in.component.jsx
+++ src/routes/signin/sign-in.component.jsx
@@ -3,6 +3,8 @@ import {
    createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const SignIn = () => {
 
    const logGoogleUser = async () => {
@@ -13,9 +15,8 @@ const SignIn = () => {
    return (
       <div>
          <h1>Sign In Page</h1>
-         <button onClick={logGoogleUser}>
-            Sign in with Google
-         </button>
+         <button onClick={logGoogleUser}>Sign in with Google</button>
+         <SignUpForm/>
       </div>
    );
 }
