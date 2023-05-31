0350ccfa91969bb0b81a314a7f5e1838b5f19012
emrahbeydilli <emrahbeydilli@gmail.com>
2023-05-31 11:02:05 +0300

    adding add to cart func

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,20 @@
-import Button from '../button/button.component';
+import { useContext } from "react";
 
+import { CartContext } from "../../contexts/cart.context";
+
+import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CardDropdown = () => {
+    const {cartItems} = useContext(CartContext);
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                
+                {cartItems.map(item => (
+                    <CartItem key={item.id} cartItem={item}/>
+                ))}
             </div>
             <Button>Go to Checkout</Button>
         </div>
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
@@ -21,5 +21,5 @@ root.render(
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
