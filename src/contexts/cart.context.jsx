8a3eddcca749c70ecd43d754aafbdf42c5e56d0f
emrahbeydilli <emrahbeydilli@gmail.com>
2023-06-08 23:38:40 +0300

    replacing state to reducer for context

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useReducer } from 'react';
+
+import { createAction } from '../utils/reducer/reducer.utils';
 
 const addCartItem = (cartItems, productToAdd) => {
     // find if cartItems contains productToAdd
@@ -36,7 +38,7 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     );
 }
 
-const clearCartItem= (cartItems,cartItemToClear) => {
+const clearCartItem = (cartItems, cartItemToClear) => {
     return cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);
 }
 
@@ -51,35 +53,76 @@ export const CartContext = createContext({
     cartTotal: 0,
 });
 
+const CART_ACTION_TYPES = {
+    SET_CART_ITEMS: 'SET_CART_ITEMS',
+    SET_IS_CART_OPEN: 'SET_IS_CART_OPEN',
+    
+}
+
+const INITIAL_STATE = {
+    isCartopen: false,
+    cartItems: [],
+    cartCount: 0,
+    cartTotal: 0
+};
+
+const cartReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case CART_ACTION_TYPES.SET_CART_ITEMS:
+            return {
+                ...state,
+                ...payload,
+            }
+        case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+            return{
+                ...state,
+                isCartOpen:payload,
+            }
+
+        default:
+            throw new Error('Unhandled type ${type} in cartReducer');
+    }
+}
+
+
 export const CartProvider = ({ children }) => {
-    const [isCartOpen, setIsCartOpen] = useState(false);
-    const [cartItems, setCartItems] = useState([]);
-    const [cartCount, setCartCount] = useState(0);
-    const [cartTotal,setCartTotal] = useState(0);
+    const [{cartItems, cartTotal, cartCount, isCartOpen}, dispatch] = useReducer(cartReducer, INITIAL_STATE);
 
-    useEffect(() => {
-        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
-        setCartCount(newCartCount);
-    }, [cartItems]);
+    const updateCartitemsReducer = (newCartItems) => {
+        const newCartCount = newCartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+        const newCartTotal = newCartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0);
 
-    useEffect(() => {
-        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity*cartItem.price, 0);
-        setCartTotal(newCartTotal);
-    }, [cartItems]);
+        dispatch(
+            createAction(CART_ACTION_TYPES.SET_CART_ITEMS,{
+                cartItems: newCartItems,
+                cartTotal: newCartTotal,
+                cartCount: newCartCount
+            })
+        );
+    }
 
+    
     const addItemToCart = (productToAdd) => {
-        setCartItems(addCartItem(cartItems, productToAdd));
+        const newCartItems = addCartItem(cartItems, productToAdd);
+        updateCartitemsReducer(newCartItems);
     }
-
+    
     const removeItemFromCart = (cartItemToRemove) => {
-        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+        const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+        updateCartitemsReducer(newCartItems);
     }
-
+    
     const clearItemFromCart = (cartItemToClear) => {
-        setCartItems(clearCartItem(cartItems, cartItemToClear));
+        const newCartItems = clearCartItem(cartItems, cartItemToClear);
+        updateCartitemsReducer(newCartItems);
     }
-
-
+    
+    const setIsCartOpen = (bool) =>{
+        dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN,bool));
+    }
+    
     const value = {
         isCartOpen,
         setIsCartOpen,
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,19 +1,54 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
+
+import { createAction } from '../utils/reducer/reducer.utils';
 
 // as the actual value you want to access
 export const UserContext = createContext({
-    currentUser: null,
     setCurrentUser: () => null,
+    currentUser: null,
 });
 
-// for state functionality, component itself
-export const UserProvider = ({children}) =>{
-    const [currentUser,setCurrentUser] = useState(null);
-    const value = {currentUser, setCurrentUser};
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER',
+}
 
-    useEffect(()=>{
-        const unsubcribe = onAuthStateChangedListener((user)=>{
+// Reducer Function
+const userReducer = (state, action) => {
+    console.log('dispacthed',action);
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload,
+            }
+
+        default:
+            throw new Error('Unhandled type ${type} in userReducer');
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null,
+};
+
+// for state functionality, component itself
+export const UserProvider = ({ children }) => {
+    const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    console.log(currentUser);
+
+    const setCurrentUser = (user) => {
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER,user));
+    }
+
+    
+    
+    const value = { currentUser, setCurrentUser };
+    
+    useEffect(() => {
+        const unsubcribe = onAuthStateChangedListener((user) => {
             if (user) {
                 createUserDocumentFromAuth(user);
             }
@@ -21,7 +56,18 @@ export const UserProvider = ({children}) =>{
         });
 
         return unsubcribe;
-    },[]);
+    }, []);
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+};
+
+/* 
+
+const userReducer = (state,action) => {
+
+    return {
+        currentUser:null,
+    }
 }
+
+*/
