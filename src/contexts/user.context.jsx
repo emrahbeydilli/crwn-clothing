4bcb3c11c713c2bb7c44c2b1a4ee18bb911f6357
Emrah Beydilli <emrahbeydilli@gmail.com>
2023-06-09 21:13:32 +0300

    redux reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -9,54 +9,16 @@ export const UserContext = createContext({
     currentUser: null,
 });
 
-export const USER_ACTION_TYPES = {
-    SET_CURRENT_USER: 'SET_CURRENT_USER',
-}
 
-// Reducer Function
-const userReducer = (state, action) => {
-    console.log('dispacthed',action);
-    const { type, payload } = action;
-
-    switch (type) {
-        case USER_ACTION_TYPES.SET_CURRENT_USER:
-            return {
-                ...state,
-                currentUser: payload,
-            }
-
-        default:
-            throw new Error('Unhandled type ${type} in userReducer');
-    }
-}
-
-const INITIAL_STATE = {
-    currentUser: null,
-};
 
 // for state functionality, component itself
 export const UserProvider = ({ children }) => {
     const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
-    console.log(currentUser);
 
-    const setCurrentUser = (user) => {
-        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER,user));
-    }
 
     
     
     const value = { currentUser, setCurrentUser };
-    
-    useEffect(() => {
-        const unsubcribe = onAuthStateChangedListener((user) => {
-            if (user) {
-                createUserDocumentFromAuth(user);
-            }
-            setCurrentUser(user);
-        });
-
-        return unsubcribe;
-    }, []);
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context'; 
-import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
