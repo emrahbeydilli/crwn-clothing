2aee381f4449b6ca0a8591bc6d0725160cd0cf1a
emrahbeydilli <emrahbeydilli@gmail.com>
2023-06-11 12:00:17 +0300

    adding categories redux store

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,7 +1,6 @@
 import { useContext, useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 
-import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 
 import './category.styles.scss'
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,11 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import {setCategoriesMap} from '../../store/categories/categories.action';
 
 import './shop.styles.scss';
 
 const Shop = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            dispatch(setCategoriesMap(categoryMap));
+        }
+        getCategoriesMap();
+    }, []);
 
     return (
         <Routes>
